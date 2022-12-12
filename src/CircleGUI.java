d2ab2c333bee54fe9d6dc4dc0a2f0abd6378ac7f
Czar Philip <czarphilip00@gmail.com>
2022-12-12 14:25:10 +0800

    Final

--- src/CircleGUI.java
+++ src/CircleGUI.java
@@ -1,12 +1,45 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CircleGUI extends JFrame{
+public class CircleGUI extends JFrame {
     private JPanel pnlMain;
     private JTextField tfRadius;
     private JTextField tfArea;
     private JTextField tfCircumference;
     private JButton btnClear;
     private JButton btnCompute;
+
+    // Constructor/
+    public CircleGUI() {
+        // Set up the GUI components
+        // Add action listeners to the buttons
+        btnCompute.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // Get the radius value from the text field
+                double radius = Double.parseDouble(tfRadius.getText());
+
+                // Calculate the area and circumference
+                double area = Math.PI * radius * radius;
+                double circumference = 2 * Math.PI * radius;
+
+                // Set the calculated values in the text fields
+                tfArea.setText(String.format("%.2f", area));
+                tfCircumference.setText(String.format("%.2f", circumference));
+            }
+        });
+
+        btnClear.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                // Clear the text fields
+                tfRadius.setText("");
+                tfArea.setText("");
+                tfCircumference.setText("");
+            }
+        });
+    }
 
     public static void main(String[] args) {
         CircleGUI app = new CircleGUI();
